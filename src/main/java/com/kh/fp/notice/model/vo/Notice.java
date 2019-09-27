82ee138f4df74f2853a6984982b127385be34487
rudgu <rudgu@DESKTOP-HHAO0UD>
2019-09-27 10:04:36 +0900

    Min

--- src/main/java/com/kh/fp/notice/model/vo/Notice.java
+++ src/main/java/com/kh/fp/notice/model/vo/Notice.java
@@ -15,13 +15,12 @@ public class Notice implements Serializable{
 	private int writer;
 	private String className;
 	private String picture;
+	private String userName;
 	
 	public Notice() {}
 
-
 	public Notice(int boardNum, String boardTitle, String boardContent, Date boardDate, String boardtype, int classNum,
-
-			int kinderNum, int writer, String className, String picture) {
+			int kinderNum, int writer, String className, String picture, String userName) {
 		super();
 		this.boardNum = boardNum;
 		this.boardTitle = boardTitle;
@@ -32,118 +31,107 @@ public class Notice implements Serializable{
 		this.kinderNum = kinderNum;
 		this.writer = writer;
 		this.className = className;
-
 		this.picture = picture;
+		this.userName = userName;
 	}
-
 
 	public int getBoardNum() {
 		return boardNum;
 	}
 
-
 	public void setBoardNum(int boardNum) {
 		this.boardNum = boardNum;
 	}
-
 
 	public String getBoardTitle() {
 		return boardTitle;
 	}
 
-
 	public void setBoardTitle(String boardTitle) {
 		this.boardTitle = boardTitle;
 	}
-
 
 	public String getBoardContent() {
 		return boardContent;
 	}
 
-
 	public void setBoardContent(String boardContent) {
 		this.boardContent = boardContent;
 	}
-
 
 	public Date getBoardDate() {
 		return boardDate;
 	}
 
-
 	public void setBoardDate(Date boardDate) {
 		this.boardDate = boardDate;
 	}
-
 
 	public String getBoardtype() {
 		return boardtype;
 	}
 
-
 	public void setBoardtype(String boardtype) {
 		this.boardtype = boardtype;
 	}
-
 
 	public int getClassNum() {
 		return classNum;
 	}
 
-
 	public void setClassNum(int classNum) {
 		this.classNum = classNum;
 	}
-
 
 	public int getKinderNum() {
 		return kinderNum;
 	}
 
-
 	public void setKinderNum(int kinderNum) {
 		this.kinderNum = kinderNum;
 	}
-
 
 	public int getWriter() {
 		return writer;
 	}
 
-
 	public void setWriter(int writer) {
 		this.writer = writer;
 	}
-
 
 	public String getClassName() {
 		return className;
 	}
 
-
 	public void setClassName(String className) {
 		this.className = className;
 	}
-
 
 	public String getPicture() {
 		return picture;
 	}
 
-
 	public void setPicture(String picture) {
 		this.picture = picture;
-
 	}
 
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
 
 	@Override
 	public String toString() {
 		return "Notice [boardNum=" + boardNum + ", boardTitle=" + boardTitle + ", boardContent=" + boardContent
 				+ ", boardDate=" + boardDate + ", boardtype=" + boardtype + ", classNum=" + classNum + ", kinderNum="
-				+ kinderNum + ", writer=" + writer + ", className=" + className + ", picture=" + picture + "]";
+				+ kinderNum + ", writer=" + writer + ", className=" + className + ", picture=" + picture + ", userName="
+				+ userName + "]";
 	}
+
+
 
 
 	
